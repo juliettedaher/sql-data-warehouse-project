772473bbcfdd774b8bca7f6a087bdffe9edcaf72
juliettedaher <juliettejdaher@gmail.com>
2025-09-08 10:27:01 +0300

    Create product.cleaning_bronze.sql

---
+++ scripts/bronze/bronze.cleaning/product.cleaning_bronze.sql
@@ -0,0 +1,27 @@
+
+use datawarehouse;
+INSERT INTO silver.crm_prd_info (
+    prd_id,
+    prd_key,
+    prd_cost,
+    prd_start_dt,
+    prd_end_dt,
+    prd_line,
+    cat_id,
+    prd_nm
+)
+SELECT
+    prd_id,
+    SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,  -- extract prd_key
+    ISNULL(prd_cost, 0) AS prd_cost,                -- replace NULL with 0
+    CAST(prd_start_dt AS date) AS prd_start_dt,
+    CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS date) AS prd_end_dt,
+    CASE 
+        WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'MOUNTAIN'
+        WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'OTHER SALES'
+        WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'ROAD'
+        WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'TOURING'
+    END AS prd_line,
+    REPLACE(SUBSTRING(prd_key, 1, 1), '_', '_') AS cat_id, -- your category
+    prd_nm
+FROM bronze.crm_prd_info;
