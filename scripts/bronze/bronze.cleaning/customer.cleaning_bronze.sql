a996651806ee3837de295caf8d31006e7a3461bd
juliettedaher <juliettejdaher@gmail.com>
2025-09-08 10:25:19 +0300

    Create customer.cleaning_bronze.sql

---
+++ scripts/bronze/bronze.cleaning/customer.cleaning_bronze.sql
@@ -0,0 +1,31 @@
+use datawarehouse;
+
+insert into silver.crm_cust_info(
+cst_id,
+cst_create_date,
+cst_firstname,
+cst_lastname,
+cst_gndr,
+cst_marital_status,
+cst_key)
+
+select 
+cst_id,
+cst_create_date,
+trim(cst_firstname) as cst_firstname,
+trim(cst_lastname) as cst_lastname,
+--we removed the unwanted spaces from those 2 columns--
+case when UPPER(TRIM(cst_gndr))='F' THEN 'Female'
+     when UPPER(TRIM(cst_gndr))='M' THEN 'Male'
+     ELSE 'N/A'
+END cst_gndr,
+--data standardazarion and consistency--
+cst_key,
+case when UPPER(TRIM(cst_marital_status))='S' THEN 'SINGLE'
+     when UPPER(TRIM(cst_marital_status))='M' THEN 'MARRIED'
+     ELSE 'N/A'
+END cst_gndr
+from(
+select *, ROW_NUMBER() over (partition by cst_id order by cst_create_date desc) as flag_last
+from bronze.crm_cust_info)
+t where flag_last=1
