67a98642161f11617e588e2fdfbe0f039011eefa
juliettedaher <juliettejdaher@gmail.com>
2025-09-10 22:33:43 +0300

    Update customer.cleaning_bronze.sql

--- scripts/bronze/bronze.cleaning/customer.cleaning_bronze.sql
+++ scripts/bronze/bronze.cleaning/customer.cleaning_bronze.sql
@@ -1,5 +1,7 @@
 use datawarehouse;
-
+truncate table  silver.crm_cust_info;
+select * from silver.crm_cust_info;
+select * from bronze.crm_cust_info;
 insert into silver.crm_cust_info(
 cst_id,
 cst_create_date,
@@ -9,23 +11,29 @@ cst_gndr,
 cst_marital_status,
 cst_key)
 
+
 select 
 cst_id,
 cst_create_date,
 trim(cst_firstname) as cst_firstname,
 trim(cst_lastname) as cst_lastname,
+
 --we removed the unwanted spaces from those 2 columns--
 case when UPPER(TRIM(cst_gndr))='F' THEN 'Female'
      when UPPER(TRIM(cst_gndr))='M' THEN 'Male'
-     ELSE 'N/A'
-END cst_gndr,
+     ELSE 'n/a'
+END  as cst_gndr,
+
 --data standardazarion and consistency--
-cst_key,
+
 case when UPPER(TRIM(cst_marital_status))='S' THEN 'SINGLE'
      when UPPER(TRIM(cst_marital_status))='M' THEN 'MARRIED'
      ELSE 'N/A'
-END cst_gndr
+end as cst_marital_status,
+cst_key
 from(
 select *, ROW_NUMBER() over (partition by cst_id order by cst_create_date desc) as flag_last
 from bronze.crm_cust_info)
+
 t where flag_last=1
+  and cst_id is not null;
