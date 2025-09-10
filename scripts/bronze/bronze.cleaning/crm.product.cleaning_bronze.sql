d5c54bd09173e391b9f3c4a0ddd7bfe846eeaa16
juliettedaher <juliettejdaher@gmail.com>
2025-09-10 22:35:09 +0300

    Update and rename product.cleaning_bronze.sql to crm.product.cleaning_bronze.sql

--- scripts/bronze/bronze.cleaning/crm.product.cleaning_bronze.sql
+++ scripts/bronze/bronze.cleaning/crm.product.cleaning_bronze.sql
@@ -1,17 +1,18 @@
-
 use datawarehouse;
 INSERT INTO silver.crm_prd_info (
     prd_id,
+    cat_id,
     prd_key,
     prd_cost,
     prd_start_dt,
     prd_end_dt,
     prd_line,
-    cat_id,
+    
     prd_nm
 )
 SELECT
     prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- your category
     SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,  -- extract prd_key
     ISNULL(prd_cost, 0) AS prd_cost,                -- replace NULL with 0
     CAST(prd_start_dt AS date) AS prd_start_dt,
@@ -22,6 +23,5 @@ SELECT
         WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'ROAD'
         WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'TOURING'
     END AS prd_line,
-    REPLACE(SUBSTRING(prd_key, 1, 1), '_', '_') AS cat_id, -- your category
     prd_nm
 FROM bronze.crm_prd_info;
